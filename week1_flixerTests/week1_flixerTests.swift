b0e4132a5af799d96619fe38379f8749b027404d
Ethan Chen <ktun7@mail.ccsf.edu>
2018-10-25 14:01:47 -0700

    Initial Commit

---
+++ week1_flixerTests/week1_flixerTests.swift
@@ -0,0 +1,34 @@
+//
+//  week1_flixerTests.swift
+//  week1_flixerTests
+//
+//  Created by Ethan Chen on 10/25/18.
+//  Copyright © 2018 kyaw tun. All rights reserved.
+//
+
+import XCTest
+@testable import week1_flixer
+
+class week1_flixerTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
